3bc4f05368b940eccc70b33974334082a773db5b
Hieuiutatcamn <conghieu2k4@gmail.com>
2024-11-29 08:28:39 +0700

    Bài tập cá nhân LTTD _ TranCongHieu_22115053122113

---
+++ app/src/main/java/com/example/tranconghieu_22115053122113_btcanhancuoiki/Main_activity.java
@@ -0,0 +1,12 @@
+package com.example.tranconghieu_22115053122113_btcanhancuoiki;
+
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main_activity extends AppCompatActivity {
+
+
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TranCongHieu_22115053122113_BTcanhancuoiki"
+include(":app")
+ 
